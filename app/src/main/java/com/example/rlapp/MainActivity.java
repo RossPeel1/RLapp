e0ac9a797f67df4d7cb6e49b88dcd3c67f435c89
Ross <ross@festivalglass.co.uk>
2019-03-10 14:40:34 +0000

    added activities for each menu title and given each an intent that starts a new activity when selected. need to a menu/toolbar for each.

--- app/src/main/java/com/example/rlapp/MainActivity.java
+++ app/src/main/java/com/example/rlapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rlapp;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -9,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         navigationView = (NavigationView) findViewById(R.id.navigation_View);
         View navView = navigationView.inflateHeaderView(R.layout.header);
 
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -32,27 +35,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    private void UserMenuSelector(MenuItem menuItem) {
+    private void UserMenuSelector(MenuItem menuItem) {                                              // allows the user to select a feature on the app which starts the corresponding activity.
         switch (menuItem.getItemId()){
-
             case R.id.nav_PoochProfile:
                 Toast.makeText(getApplicationContext(), "Pooch profile", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, Pooch_Profile.class);
+                startActivity(intent);
                 break;
             case R.id.nav_home:
                 Toast.makeText(getApplicationContext(),"Home", Toast.LENGTH_SHORT).show();
+                Intent homeIntent = new Intent(this, Home.class );
+                startActivity(homeIntent);
                 break;
             case R.id.nav_Post:
                 Toast.makeText(getApplicationContext(),"Post", Toast.LENGTH_SHORT).show();
+                Intent postIntent = new Intent(this, Post.class);
+                startActivity(postIntent);
                 break;
             case R.id.nav_friends:
                 Toast.makeText(getApplicationContext(),"Friends", Toast.LENGTH_SHORT).show();
+                Intent fiendsIntent = new Intent(this, Friends.class);
+                startActivity(fiendsIntent);
                 break;
             case R.id.nav_findFriend:
                 Toast.makeText(getApplicationContext(),"Find Friends", Toast.LENGTH_SHORT).show();
+                Intent findFriendsIntent = new Intent(this, FindFriends.class);
+                startActivity(findFriendsIntent);
                 break;
             case R.id.nav_messages:
                 Toast.makeText(getApplicationContext(),"Messages", Toast.LENGTH_SHORT).show();
+                Intent messageIntent = new Intent(this, Message.class);
+                startActivity(messageIntent);
+                startActivity(messageIntent);
                 break;
             case R.id.nav_settings:
                 Toast.makeText(getApplicationContext(),"settings", Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/com/example/rlapp/Pooch_Profile.java
@@ -0,0 +1,14 @@
+package com.example.rlapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pooch_Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pooch__profile);
+
+    }
+}

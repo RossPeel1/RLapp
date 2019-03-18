e339624af436dd8c8988ae5446ebb6995b2516d1
Ross <ross@festivalglass.co.uk>
2019-03-18 19:22:41 +0000

    updated google discrepancies and and added a setup activity.

--- app/src/main/java/com/example/rlapp/Login.java
+++ app/src/main/java/com/example/rlapp/Login.java
@@ -1,13 +1,39 @@
 package com.example.rlapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-
+private Button loginButton;
+private EditText userEmail, userPassword;
+private TextView createAcountTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        createAcountTxt = (TextView) findViewById(R.id.create_account_txt);
+        userEmail = (EditText) findViewById(R.id.e_mail_input_txt);
+        userPassword = (EditText) findViewById(R.id.password_input_txt);
+        loginButton = (Button) findViewById(R.id.login_btn);
+
+        createAcountTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendUserToRegisterActivity();
+            }
+        });
+    }
+
+    private void sendUserToRegisterActivity() {
+        Intent registerIntent  = new Intent(Login.this, Register.class);
+        startActivity(registerIntent);
+
+
     }
 }
--- app/src/main/java/com/example/rlapp/MainActivity.java
+++ app/src/main/java/com/example/rlapp/MainActivity.java
@@ -13,6 +13,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     private NavigationView navigationView;
@@ -20,11 +24,13 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private Toolbar mainToolbar;
     private ActionBarDrawerToggle actionBarDrawerToggle;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();                                                         // initialise authentication for freebase
 
         mainToolbar = (Toolbar) findViewById(R.id.main_page_Toolbar);                               // setup menu bar layout
         setSupportActionBar(mainToolbar);
@@ -46,6 +52,24 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null)
+        {
+            SendUserToLoginActivity();
+        }
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(MainActivity.this, Login.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);      // validation
+        startActivity(loginIntent);
+        finish();
     }
 
     @Override

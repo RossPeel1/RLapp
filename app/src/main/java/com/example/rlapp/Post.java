1c6b62d0effee59e87457936ea3745d85881e1a7
Ross <ross@festivalglass.co.uk>
2019-09-25 22:39:13 +0100

    app bar created and user can move through main and post activities

--- app/src/main/java/com/example/rlapp/Post.java
+++ app/src/main/java/com/example/rlapp/Post.java
@@ -1,13 +1,43 @@
 package com.example.rlapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Post extends AppCompatActivity {
 
+    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mToolbar = (Toolbar)findViewById(R.id.post_page_Toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Update Post");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(getApplicationContext(), "Pooch profile", Toast.LENGTH_SHORT).show();
+        int id = item.getItemId();
+        if (id == android.R.id.home)
+        {
+
+            sendUserToMainActivity();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void sendUserToMainActivity()
+    {
+        Intent MainIntent = new Intent(Post.this, MainActivity.class);
+        startActivity(MainIntent);
     }
 }
